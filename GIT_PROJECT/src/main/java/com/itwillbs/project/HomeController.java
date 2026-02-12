7c872dbd4bd27ae4f3f028beaaf71d844679ccb2
ITWILL <ITWILL@ITWILL5-12>
2026-02-12 16:48:08 +0900

    커밋 테스트용.

--- GIT_PROJECT/src/main/java/com/itwillbs/project/HomeController.java
+++ GIT_PROJECT/src/main/java/com/itwillbs/project/HomeController.java
@@ -21,6 +21,7 @@ public class HomeController {
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
+	 * 테스트 커밋 홈 컨트롤러
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {

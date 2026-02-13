2d5e597c1594ca7051bf0004c30b071f7530bc77
ITWILL <ITWILL@ITWILL5-2>
2026-02-13 17:12:10 +0900

    커밋 실험

---
+++ GIT_PROJECT/src/main/java/com/itwillbs/project/job/dto/JobDTO.java
@@ -0,0 +1,15 @@
+package com.itwillbs.project.job.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class JobDTO {
+
+
+}
---
+++ GIT_PROJECT/src/main/java/com/itwillbs/project/job/mapper/JobMapper.java
@@ -0,0 +1,8 @@
+package com.itwillbs.project.job.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public class JobMapper {
+
+}

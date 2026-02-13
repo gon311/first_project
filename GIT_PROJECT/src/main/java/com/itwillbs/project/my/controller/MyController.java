f13b4a468bde05f2da08aa88ba9f6826d5afa915
Jaeoe <ITWILL@ITWILL5-12>
2026-02-13 17:08:06 +0900

    my페이지 jsp 추가

--- GIT_PROJECT/src/main/java/com/itwillbs/project/my/controller/MyController.java
+++ GIT_PROJECT/src/main/java/com/itwillbs/project/my/controller/MyController.java
@@ -1,13 +1,22 @@
 package com.itwillbs.project.my.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 import lombok.extern.log4j.Log4j2;
 
 @Controller
-@RequestMapping("/regist")
+@RequestMapping("/my")
 @Log4j2
 public class MyController {
 	
+	@GetMapping("/my")
+	public String myMy() {
+		
+		return "/my/my";
+	}
 }
--- GIT_PROJECT/src/main/java/com/itwillbs/project/my/service/MyService.java
+++ GIT_PROJECT/src/main/java/com/itwillbs/project/my/service/MyService.java
@@ -6,3 +6,7 @@ import org.springframework.stereotype.Service;
 public class MyService {
 
 }
+
+
+
+

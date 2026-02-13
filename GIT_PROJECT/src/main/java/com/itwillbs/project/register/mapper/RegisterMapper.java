c90e77edc67d9a520b87c2c8e1ec7ff071dd517a
ITWILL <ITWILL@ITWILL5-25>
2026-02-13 16:30:37 +0900

    기초적인 임플란트

---
+++ GIT_PROJECT/src/main/java/com/itwillbs/project/register/mapper/RegisterMapper.java
@@ -0,0 +1,8 @@
+package com.itwillbs.project.register.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public class RegisterMapper {
+
+}
---
+++ GIT_PROJECT/src/main/java/com/itwillbs/project/register/service/RegisterService.java
@@ -0,0 +1,8 @@
+package com.itwillbs.project.register.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegisterService {
+
+}

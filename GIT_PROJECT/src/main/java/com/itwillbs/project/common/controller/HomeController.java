46ebb18f4fa14de8f7e90e5fd85f1d19ec4bb426
LDG <gon311@knou.ac.kr>
2026-02-20 15:52:18 +0900

    이다겸 : HomeController 위치 변경

--- GIT_PROJECT/src/main/java/com/itwillbs/project/common/controller/HomeController.java
+++ GIT_PROJECT/src/main/java/com/itwillbs/project/common/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.itwillbs.project;
+package com.itwillbs.project.common.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;

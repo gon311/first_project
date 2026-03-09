1d5f38b9e761fe858b060ec91c1fcdb6c0297910
LDG <gon311@knou.ac.kr>
2026-03-09 15:36:44 +0900

    이다겸 : common-dto 삭제된거 복구

--- GIT_PROJECT/src/main/java/com/itwillbs/project/common/dto/FileDTO.java
+++ GIT_PROJECT/src/main/java/com/itwillbs/project/common/dto/FileDTO.java
@@ -1,27 +1,6 @@
 package com.itwillbs.project.common.dto;
 
 import java.time.LocalDateTime;
-package com.itwillbs.project.common.dto;
-
-import java.time.LocalDate;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-@Getter
-@Setter
-@ToString
-public class CompanyCardDTO {
-	private String title;
-	private String companyName;
-	private String salary;
-	@JsonFormat(pattern = "yyyy-MM-dd")
-	private LocalDate closeDate;
-	private Long jobId; 
-}
 
 import lombok.Getter;
 import lombok.Setter;
@@ -39,5 +18,5 @@ public class FileDTO {
 	private String filePath;
 	private Long fileSize;
 	private String fileExt;
-	private LocalDateTime createdAt;
+	private LocalDateTime createdAt;   
 }

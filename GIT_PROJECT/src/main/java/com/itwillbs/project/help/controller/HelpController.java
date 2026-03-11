e72a011a88d3732b56da1678116c8e57ede33cc0
Sooyeon Bree <sooyeon22@ewha.ac.kr>
2026-03-11 14:32:54 +0900

    공지사항 게시판 상세 정보 오류 수정

--- GIT_PROJECT/src/main/java/com/itwillbs/project/help/controller/HelpController.java
+++ GIT_PROJECT/src/main/java/com/itwillbs/project/help/controller/HelpController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.itwillbs.project.admin.service.AdminService;
 import com.itwillbs.project.help.dto.NoticeDTO;
 import com.itwillbs.project.help.service.HelpService;
 
@@ -22,6 +23,7 @@ import lombok.extern.log4j.Log4j2;
 public class HelpController {
 	@Autowired
 	private HelpService helpService;
+	private AdminService adminService;
 	
 	@GetMapping("/helpWord")
 	public String posting() {
@@ -43,7 +45,7 @@ public class HelpController {
 	
 	@GetMapping("/noticeDetail")
 	public String noticeDetail(@RequestParam("noticeId") int noticeId, Model model) {
-		NoticeDTO noticeDTO = helpService.getNoticeDetail(noticeId);
+		com.itwillbs.project.admin.dto.NoticeDTO noticeDTO = adminService.getNoticeDetail(noticeId);
 		model.addAttribute("noticeDTO", noticeDTO); 
 		System.out.println("데이터 결과값: " + noticeDTO.toString());
 		return "help/noticeDetail";
